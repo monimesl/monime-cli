9c9a6da3e306aab3c0545b47f2eba776f51b8810
Alpha Shaw <shawalpha5@gmail.com>
2025-05-28 11:50:07 +0000

    update...

--- cmds/space/activate.go
+++ cmds/space/activate.go
@@ -11,9 +11,12 @@ import (
 )
 
 var activateCmd = &cobra.Command{
-	Use:   "activate",
-	Short: "",
-	Long:  ``,
+	Use:   "activate <space-id>",
+	Short: "Activate a Space by its ID or alias.\n",
+	Long: `This command sets the specified space as your current working context.
+Subsequent operations (like managing webhooks or more) will be executed within the activated space.
+
+You must provide a valid space ID or alias as an argument.`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		if len(args) < 1 {
 			text.PrintError(os.Stderr, "\033[1;31mMissing space ID or alias.\033[0m")
