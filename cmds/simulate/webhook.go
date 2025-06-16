2876bc43a74992225cf6d165cb048f19cf76ba3b
Alpha Shaw <shawalpha5@gmail.com>
2025-06-16 12:18:02 +0000

    update...

--- cmds/simulate/webhook.go
+++ cmds/simulate/webhook.go
@@ -9,7 +9,7 @@ var webhookSimulator = &cobra.Command{
 	Use:   "webhook",
 	Short: "Open the Webhook simulator",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		c := exec.Command("apps/webhook/build/bin/webhook.app/Contents/MacOS/webhook")
+		c := exec.Command("open /Users/alphashaw/projects/Projects/sources/monimesl/apps/monime-cli/apps/webhook/build/bin/webhook.app")
 		return c.Run()
 	},
 }
