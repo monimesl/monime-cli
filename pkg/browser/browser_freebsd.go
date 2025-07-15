77d820967067f7885010170deed426f3b59ce9c4
Alpha Shaw <shawalpha5@gmail.com>
2025-07-15 11:02:05 +0000

    apply large-scale changes

--- pkg/browser/browser_freebsd.go
+++
@@ -1,9 +0,0 @@
-package browser
-
-func open(url string) (Command, error) {
-	err := runCmd("xdg-open", url)
-	if e, ok := err.(*exec.Error); ok && e.Err == exec.ErrNotFound {
-		return errors.New("xdg-open: command not found - install xdg-utils from ports(8)")
-	}
-	return err
-}
