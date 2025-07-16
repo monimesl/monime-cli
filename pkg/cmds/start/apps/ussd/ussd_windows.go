95b087c6dbfc5e035e76bd14d65f583ce3fda341
Alpha Shaw <shawalpha5@gmail.com>
2025-07-16 06:41:10 +0000

    Added placeholder implementation for `launchApp` on Windows in the USSD package.

---
+++ pkg/cmds/start/apps/ussd/ussd_windows.go
@@ -0,0 +1,11 @@
+package ussd
+
+import (
+	"context"
+	"errors"
+	"os"
+)
+
+func launchApp(ctx context.Context) (*os.Process, error) {
+	return nil, errors.New("windows launchApp not implemented")
+}
